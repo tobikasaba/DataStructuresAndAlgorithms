dce039214fa288327914905997d28f725f6a4e79
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-06-25 14:18:33 +0100

    Big O Different terms for inputs

--- BigO/big_O.py
+++ BigO/big_O.py
@@ -1,9 +1,10 @@
 # This piece of code is know as O(n) where n is the number of operations.
 # Hence, in this case, there were 10 Operations
 def print_items(n):
+    # O(n)
     for i in range(n):
         print(i)
-
+    # O(n)
     # drop constant
     for j in range(n):
         print(j)
@@ -25,10 +26,30 @@ def print_items_two(n):
     for k in range(n):
         print(k)
 
+
 print(print_items_two(10))
 
+
 # O(1)
-# The number of operations remanins the same regardless of the value of n
+# The number of operations remains the same regardless of the value of n
 # this is also known as constant time meaning as n increases the number of operations will remain constant
-def add_itmes(n):
-    return n+n
+def add_items(n):
+    return n + n
+
+
+# Different terms for inputs
+# because this functions has two different parameters, it can't be O(n+n) => O(2n) => 0(n)
+# essentially, you cant drop a constant because they are different from the parameter
+# it becomes O(a+b) and nested loops O(a*b)
+def print_items_diff_inputs(a, b):
+    # O(a)
+    for i in range(a):
+        print(i)
+    # O(b)
+    for j in range(b):
+        print(j)
+
+    # this becomes O(a*b)
+    for i in range(a):
+        for j in range(b):
+            print(a, b)
