15fe242ad87012f4127ab612c6ccafb8bfdc0871
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-06-27 20:28:01 +0100

    feat: pop - created the pop function which removes the last node from a linked list and returns the value in the node removed. - the size of the linked list is reduced - the tail node is also updated

--- LinkedList/constructor.py
+++ LinkedList/constructor.py
@@ -1,4 +1,6 @@
 class LinkedList:
+    # each node is an instance of new node which has a value and next attribute.
+    # this makes setting the next value of each node possible
     def __init__(self, value):
         # create a new node
         new_node = Node(value)
@@ -15,15 +17,34 @@ class LinkedList:
             self.tail = new_node
 
         else:
-            # this sets the pointer of the current node to the new node
+            # this sets the pointer of the current node to the new node being added
             self.tail.next = new_node
-            # this then sets the tail of the linked list to the new node
+            # this then sets the tail of the linked list to the new node being added
             self.tail = new_node
         self.length += 1
         return True
 
+    def pop(self):
+        prev = self.head
+        temp = self.head
+        if self.length == 0:
+            return None
+        while temp.next is not None:
+            prev = temp
+            temp = temp.next
+        popped_node = temp
+        self.tail = prev
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            popped_node = temp
+            self.head = None
+            self.tail = None
+        return popped_node.value
+
     def prepend(self, value):
         # create a new node
+        new_node = Node(value)
         # add Node to the beginning
         pass
 
@@ -36,6 +57,9 @@ class LinkedList:
         temp = self.head
         while temp is not None:
             print(temp.value)
+            # sets the value of temp to the value the next attribute specified in a node,
+            # for example, when a new node is instantiated, the value of the node is specified
+            # and the value of the next attribute
             temp = temp.next
 
 
@@ -43,3 +67,12 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+
+test = LinkedList(1)
+test.append(2)
+test.print_list()
+print("---------")
+print("Popped value is ", test.pop())
+print("Popped value is", test.pop())
+test.print_list()
