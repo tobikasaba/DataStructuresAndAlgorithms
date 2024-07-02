f43b25d3dbc21a7039689403a6d9922d372c52a3
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-07-02 16:43:28 +0100

    linked list exercises - finding the middle node in a linked list - included multiple solutions. solution two is more effective

---
+++ LinkedList/Exercises/find_middle_node.py
@@ -0,0 +1,55 @@
+import timeit
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+
+    def find_middle_node(self):
+        fast = self.head
+        slow = self.head
+        while fast is not None:
+            if fast == self.tail:
+                return slow
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+    # more efficient solution
+    def find_middle_node_two(self):
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+my_linked_list.append(6)
+my_linked_list.append(7)
+my_linked_list.append(8)
+
+print(my_linked_list.find_middle_node().value)
