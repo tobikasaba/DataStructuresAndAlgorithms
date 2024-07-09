62bf560c008c03a16375e3733d28630245182b93
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-07-09 21:33:52 +0100

    feat is_palindrome(): - method that checks if a DLL is a palindrome - included solution from instructor as is_palindrome_b - is_palindrome_b is faster for larger DLLs and is_palindrome_b is more efficient for smaller DLLs

---
+++ DoublyLinkedList/Exercises/palindrome_checker.py
@@ -0,0 +1,92 @@
+import timeit
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def is_palindrome(self):
+        if self.length <= 1:
+            return True
+        forward = self.head
+        reverse = self.tail
+        while forward is not None and reverse is not None:
+            if forward.value != reverse.value:
+                return False
+            forward = self.head.next
+            reverse = self.tail.next
+        return True
+
+    def is_palindrome_b(self):
+        if self.length <= 1:
+            return True
+        forward = self.head
+        reverse = self.tail
+
+        for i in range(self.length // 2):
+            if forward.value != reverse.value:
+                return False
+            forward = forward.next
+            reverse = reverse.prev
+        return True
+
+
+my_dll_1 = DoublyLinkedList(1)
+my_dll_1.append(1)
+my_dll_1.append(2)
+my_dll_1.append(2)
+my_dll_1.append(2)
+my_dll_1.append(3)
+my_dll_1.append(2)
+my_dll_1.append(2)
+my_dll_1.append(2)
+my_dll_1.append(1)
+my_dll_1.append(1)
+
+print('my_dll_1 is_palindrome:')
+print(my_dll_1.is_palindrome())
+
+my_dll_2 = DoublyLinkedList(1)
+my_dll_2.append(2)
+my_dll_2.append(3)
+
+print('\nmy_dll_2 is_palindrome:')
+print(my_dll_2.is_palindrome())
+
+time_1 = timeit.timeit(lambda: my_dll_1.is_palindrome(), number=10000000)
+time_2 = timeit.timeit(lambda: my_dll_1.is_palindrome_b(), number=10000000)
+print(f"The timing of the functions are, function one: {time_1} and function two: {time_2}")
+
+print("------")
+if time_1 < time_2:
+    print("Yours is more efficent")
+else:
+    print("Yours is less efficient")
