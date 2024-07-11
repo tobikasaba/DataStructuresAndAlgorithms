380576e495af2a202ff9c6d6188a0d8506839574
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-07-11 15:31:41 +0100

    feat: pop(): - removing the last node on a stack (from the top)

--- Stacks/constructor.py
+++ Stacks/constructor.py
@@ -26,7 +26,18 @@ class Stack:
             self.top = new_node
         self.height += 1
 
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp
+
 
 my_stack = Stack(4)
 my_stack.push(3)
+my_stack.push(15)
+my_stack.pop()
 my_stack.print_stack()
