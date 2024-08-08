3021da0d1ea704945225e7bbb1c775cc31b8040d
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-08-08 12:29:13 +0100

    implemented method for deleting nodes in a binary search Tree using recursion: - min_value(): this returns the minimum value in a tree. Used as a helper method in the delete node method - __delete_node(): a private method that deletes a given node from the tree. This also adjusts the position of nodes in the tree if needed this - delete_node(): used to delete a given node by calling __delete_node() private method

--- Recursion/recursion_trees.py
+++ Recursion/recursion_trees.py
@@ -19,7 +19,7 @@ class BinarySearchTree:
         return current_node
 
     def r_insert(self, value):
-        if self.root == None:
+        if self.root is None:
             self.root = Node(value)
         self.__r_insert(self.root, value)
 
@@ -39,7 +39,36 @@ class BinarySearchTree:
     def r_contains(self, value):
         return self.__r_contains(self.root, value)
 
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
 
+    def __delete_node(self, current_node, value):
+        if current_node is None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left is None and current_node.right is None:
+                return None
+            elif current_node.left is None:
+                current_node = current_node.right
+            elif current_node.right is None:
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
+
+    def delete_node(self, value):
+        self.root = self.__delete_node(self.root, value)
+
+
+print("------------Inserting a node-------------")
 my_tree = BinarySearchTree()
 my_tree.r_insert(2)
 my_tree.r_insert(1)
@@ -49,19 +78,31 @@ print("Root: ", my_tree.root.value)
 print("Root -> Left: ", my_tree.root.left.value)
 print("Root -> Right: ", my_tree.root.right.value)
 
-print("-------------------------")
+print("\n-------Deleting a node--------------")
+my_tree.delete_node(2)
+print("Root: ", my_tree.root.value)
+print("Root -> Left: ", my_tree.root.left.value)
+print("Root -> Right: ", my_tree.root.right)
 
+print("\n------------Checking a tree contains a node-------------")
 my_tree = BinarySearchTree()
 my_tree.r_insert(47)
 my_tree.r_insert(21)
 my_tree.r_insert(76)
 my_tree.r_insert(18)
 my_tree.r_insert(27)
-my_tree.r_insert(82)
 my_tree.r_insert(52)
+my_tree.r_insert(82)
 
 print("BST Contains 27: ")
 print(my_tree.r_contains(27))
 
 print("\nBST Contains 17: ")
 print(my_tree.r_contains(17))
+
+print("\n----------Searching for minimum value---------------")
+# the minimum value for the entire tree
+print(f"The minimum value for this tree is: {my_tree.min_value(my_tree.root)}")
+
+# the minimum value for the tree on the right-hand side of the root node
+print(f"\nThe minimum value for the sub tree is: {my_tree.min_value(my_tree.root.right)}")
