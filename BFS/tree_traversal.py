430e3d04b0bd2c84d131ad149de9ec999fe00e9a
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-08-09 19:02:10 +0100

    implementing Tree Traversal using the Depth-First Search PreOrder approach - changed the name of the files and directory to reflect tree traversal

--- BFS/tree_traversal.py
+++ BFS/tree_traversal.py
@@ -17,9 +17,23 @@ def BFS(self):
     return results
 
 
+def dfs_pre_order(self):
+    results = []
+
+    def traverse(current_node):
+        results.append(current_node.value)
+        if current_node.left is not None:
+            traverse((current_node.left))
+        if current_node.right is not None:
+            traverse(current_node.right)
+
+    traverse(self.root)
+    return results
+
+
 # this essentially makes the BFS function a method of the BinarySearchTree class even though it is defined outside it
 BinarySearchTree.BFS = BFS
-
+BinarySearchTree.dfs_pre_order = dfs_pre_order
 my_tree = BinarySearchTree()
 my_tree.insert(47)
 my_tree.insert(21)
@@ -29,4 +43,7 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
+print("-----------Depth First Search--------")
 print(my_tree.BFS())
+print("\n-------------PreOrder--------")
+print(my_tree.dfs_pre_order())
